2df390bba28ded487d2070e30afb52f086bec30f
Netdebom video <debomvideo@gmail.com>
2025-03-06 17:00:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1344736094561894460", ""], 
   mongodbUri : "mongodb+srv://debomvideo:Debom@2727@discordbot.xjlkr.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "c8211e8c9aa84e74a421c36a8ce87bd1",
   spotifyClientSecret : "8a444402cb8f4f5b8adff5dbaf367614",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "DJ Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
